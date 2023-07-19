bab07a72ea9699dae84934a31496883178135917
Marius Cornea <mcornea@redhat.com>
2023-07-19 16:19:35 +0300

    Add initial system test and dependencies

---
+++ vendor/github.com/openshift-kni/eco-goinfra/pkg/pod/network.go
@@ -0,0 +1,34 @@
+package pod
+
+import (
+	multus "gopkg.in/k8snetworkplumbingwg/multus-cni.v4/pkg/types"
+)
+
+// StaticIPAnnotation defines static ip address network annotation for pod object.
+func StaticIPAnnotation(name string, ipAddr []string) []*multus.NetworkSelectionElement {
+	return []*multus.NetworkSelectionElement{
+		{
+			Name:      name,
+			IPRequest: ipAddr,
+		},
+	}
+}
+
+// StaticIPAnnotationWithInterfaceAndNamespace defines static ip address, interface name and namespace
+// network annotation for pod object.
+func StaticIPAnnotationWithInterfaceAndNamespace(
+	name, namespace, intName string, ipAddr []string) []*multus.NetworkSelectionElement {
+	baseAnnotation := StaticIPAnnotation(name, ipAddr)
+	baseAnnotation[0].InterfaceRequest = intName
+	baseAnnotation[0].Namespace = namespace
+
+	return baseAnnotation
+}
+
+// StaticIPAnnotationWithMacAddress defines static ip address and static macaddress network annotation for pod object.
+func StaticIPAnnotationWithMacAddress(name string, ipAddr []string, macAddr string) []*multus.NetworkSelectionElement {
+	baseAnnotation := StaticIPAnnotation(name, ipAddr)
+	baseAnnotation[0].MacRequest = macAddr
+
+	return baseAnnotation
+}
